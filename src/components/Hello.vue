3c46618b861c4b94e963410e3256ba13cbd679b1
clearives <clearives@gmail.com>
2017-05-15 18:21:08 +0800

    加入publicPath

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,7 @@
 <div class="hello">
   <h1>{{ msg }}</h1>
   <div class="test">
-      <a href="#/">点我</a>
+      <a href="#/login">点我</a>
   </div>
 </div>
 </template>
@@ -19,22 +19,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less">
+<style lang="less" scoped>
 h1,
 h2 {
   font-weight: normal;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
 .test {
     font-size: 28px;
 
