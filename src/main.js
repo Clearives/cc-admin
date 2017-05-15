7e9004bc0bef76b7667343ecb8353034e287ab1d
clearives <clearives@gmail.com>
2017-05-15 18:09:17 +0800

    加入登录

--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 import router from './router';
 
 Vue.config.productionTip = false;
 
+Vue.use(ElementUI)
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Login from '@/pages/Login';
 import Hello from '@/components/Hello';
 
 Vue.use(Router);
-
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      component: Login,
+      name: '',
+      hidden: true,
+    },
     {
       path: '/',
       name: 'Hello',
