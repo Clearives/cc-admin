8eccd60f3c35367066a11db9cacb22174c487393
clearives <clearives@gmail.com>
2017-05-16 14:48:26 +0800

    mock数据

---
+++ src/mock/data/login.js
@@ -0,0 +1,14 @@
+import Mock from 'mockjs';
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'clearives',
+    password: '123456',
+    avatar: 'https://avatars1.githubusercontent.com/u/8925862?v=3&s=460',
+    name: '曾祥辉'
+  }
+];
+
+const Users = [];
+
+export { LoginUsers, Users };
