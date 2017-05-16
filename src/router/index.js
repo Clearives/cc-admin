4b2a97e6521a6213703c92607d913d73f8ee0867
clearives <clearives@gmail.com>
2017-05-16 11:41:25 +0800

    g router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/pages/Login';
 import Hello from '@/components/Hello';
+import Home from '@/pages/Home';
 
 Vue.use(Router);
 export default new Router({
@@ -10,12 +11,55 @@ export default new Router({
       path: '/login',
       component: Login,
       name: '',
-      hidden: true,
-    },
-    {
+      hidden: true
+    }, {
       path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
-  ],
+      component: Home,
+      name: 'Component',
+      iconCls: 'el-icon-message',
+      children: [
+        {
+          path: '/table',
+          component: Hello,
+          name: 'Table'
+        }, {
+          path: '/form',
+          component: Hello,
+          name: 'Form'
+        }
+      ]
+    }, {
+      path: '/',
+      component: Home,
+      name: 'Collection',
+      iconCls: 'el-icon-star-on',
+      children: [
+        {
+          path: '/table1',
+          component: Hello,
+          name: 'Table'
+        }, {
+          path: '/form1',
+          component: Hello,
+          name: 'Form'
+        }
+      ]
+    }, {
+      path: '/',
+      component: Home,
+      name: 'Setting',
+      iconCls: 'el-icon-setting',
+      children: [
+        {
+          path: '/table2',
+          component: Hello,
+          name: 'Table'
+        }, {
+          path: '/form2',
+          component: Hello,
+          name: 'Form'
+        }
+      ]
+    }
+  ]
 });
