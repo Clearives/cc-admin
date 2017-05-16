eb6aace6904de6fb564857e5b402ab799def7498
clearives <clearives@gmail.com>
2017-05-16 14:48:47 +0800

    加入axios mock

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios'
 import App from './App';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import 'font-awesome/css/font-awesome.min.css'
 import router from './router';
+import Mock from './mock'
+
+let useMock = location.search.indexOf('mock') > -1
+useMock && Mock.bootstrap();
 
 Vue.config.productionTip = false;
 
