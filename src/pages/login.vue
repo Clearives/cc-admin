e7df4c8c543a491df60ce3551d229c99bed8e65a
clearives <clearives@gmail.com>
2017-05-16 11:41:41 +0800

    home

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,16 +1,16 @@
 <template>
   <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-    <h3 class="title">系统登录</h3>
+    <h3 class="title">cc-admin</h3>
     <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="username"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off" placeholder="password"></el-input>
     </el-form-item>
-    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-checkbox v-model="checked" checked class="remember">Remember</el-checkbox>
     <el-form-item style="width:100%;">
-      <el-button type="primary" @click="handleSubmit" :loading="logining">登录</el-button>
-      <el-button @click="handleReset">重置</el-button>
+      <el-button type="primary" @click="handleSubmit" :loading="logining">Login</el-button>
+      <el-button @click="handleReset">Reset</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -26,10 +26,10 @@
         },
         rules: {
           account: [
-            { required: true, message: '请输入账号', trigger: 'blur' },
+            { required: true, message: 'Please input username', trigger: 'blur' },
           ],
           checkPass: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
+            { required: true, message: 'Please input password', trigger: 'blur' },
           ]
         },
         checked: true
