998c8926a4022fdcb9d0ca8bac7edd69589f6ccd
clearives <clearives@gmail.com>
2017-05-26 17:37:56 +0800

    vue组件通信

---
+++ src/pages/collection/Communication.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="com">
+    <h1>vue2 组件通信</h1>
+    <Parent/>
+  </div>
+</template>
+
+<script>
+import Parent from './communication/Parent.vue'
+export default {
+  name: 'Communication',
+  components: {
+   Parent
+ },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
