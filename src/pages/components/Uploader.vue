b999bd0936640dd24b5ee7625b70678279fe0b1d
clearives <clearives@gmail.com>
2017-06-05 15:03:02 +0800

    上传到七牛存储

--- src/pages/components/Uploader.vue
+++ src/pages/components/Uploader.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="uploader">
     <h2>上传组件</h2>
-    <vueUploader/>
+    <el-upload
+      class="avatar-uploader"
+      action="http://up-z2.qiniu.com/"
+      :show-file-list="false"
+      :data='form'
+      :on-success="handleAvatarSuccess"
+      :before-upload="beforeAvatarUpload">
+      <img v-if="imageUrl" :src="imageUrl" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
   </div>
 </template>
 
 <script>
-import vueUploader from '../../components/vue-uploader/index.vue'
+import config from '../../api/qiniu-config'
 export default {
   name: 'Uploader',
-  components: {
-    vueUploader
+  data() {
+    return {
+      imageUrl: '',
+      form: {
+        token: config.uptoken,
+        key: null
+      }
+    }
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = config.domain + '/' + file.name;
+    },
+    beforeAvatarUpload(file) {
+      this.form.key = file.name
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      // if (!isJPG) {
+      //   this.$message.error('上传头像图片只能是 JPG 格式!');
+      // }
+      if (!isLt2M) {
+        this.$message.error('上传图片大小不能超过 2MB!');
+      }
+      return isLt2M;
+    }
   }
 }
 </script>
